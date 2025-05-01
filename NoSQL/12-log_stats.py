d7d4711837d232fde5161eeb04cb593361705fbd
AlphaDesnoc <alphadesnoc@gmail.com>
2025-05-01 19:57:29 +0200

    Adding all files to the repo

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python3
-""" Module that provides some stats about Nginx logs stored in MongoDB. """
-
+"""Module for log_stats function"""
 from pymongo import MongoClient
 
 
-if __name__ == "__main__":
+def log_stats():
+    """provides some stats about Nginx logs stored in MongoDB"""
+    client = MongoClient('localhost', 27017)
+    db = client.logs
+    collection = db.nginx
 
-    client = MongoClient("mongodb://127.0.0.1:27017")
-    nginx_collection = client.logs.nginx
-
-    print(f"{nginx_collection.count_documents({})} logs\nMethods:")
-
+    logs_qty = collection.count_documents({})
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    logs_dict = {}
+    status_check = collection.count_documents({
+        "method": "GET",
+        "path": "/status"
+        })
 
+    print(f"{logs_qty} logs")
+    print("Methods:")
     for method in methods:
-        logs_dict[method] = nginx_collection.count_documents(
-            {"method": method}
-        )
+        method_qty = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {method_qty}")
+    print(f"{status_check} status check")
 
-    for method, logs in logs_dict.items():
-        print(f"\tmethod {method}: {logs}")
 
-    status_checks = nginx_collection.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
-    print(f"{status_checks} status check")
+if __name__ == "__main__":
+    log_stats()
